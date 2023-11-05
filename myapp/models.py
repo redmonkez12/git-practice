dc0c1c8162fb3ac1222429feb971a82835a83618
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2023-11-05 12:02:34 +0100

    Add last name

--- myapp/models.py
+++ myapp/models.py
@@ -8,6 +8,7 @@ class Todo(models.Model):
 
 class User(models.Model):
     first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
 
 
 class Currency(models.Model):
