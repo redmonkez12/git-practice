612963e4d471a7b75dc463e6e717af58121caf7a
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2023-11-05 11:24:46 +0100

    Add users model

--- myapp/models.py
+++ myapp/models.py
@@ -4,3 +4,12 @@ from django.db import models
 # Create your models here.
 class Todo(models.Model):
     name = models.CharField(max_length=100)
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=100)
+
+# continuous integration
+# devops
+# git actions
+# circle CI
